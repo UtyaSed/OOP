a1f70b55286a92a6552183d3cde8c9a441b8214c
UtyaSed <vbonchykv@hmail.com>
2022-02-23 19:31:07 +0200

    first master commit

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double x;
+			double y; 
+			double A = 0; 
+			double B = 0; 
+
+			Console.WriteLine("x = "); x = Convert.ToInt32(Console.ReadLine());
+
+			A = (x * x) * (Math.Sin(x/2));
+
+			// 1 
+			if (x <= -5)
+				B = Math.Atan(Math.Exp(x));
+			if (-5 < x && x <= 0)
+				B = 1 + (Math.Pow(x, 3)/4);
+			if (x > 0)
+				B = Math.Log(Math.Abs(x))-(x/5) ;
+
+			y = A + B;
+
+			Console.WriteLine("\n");
+			Console.WriteLine($"1) y = {Math.Round(y, 2)} \n");
+
+			// 2
+			if (x <= -5)
+				B = Math.Atan(Math.Exp(x));
+			else
+				if (x > 0)
+				B = Math.Log(Math.Abs(x)) - (x / 5);
+			else
+				B = 1 + (Math.Pow(x, 3) / 4);
+
+			y = A + B;
+
+			Console.WriteLine($"2) y = {y} \n");
+		}
+	}
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 1, N, k;
+
+			double S;
+
+			Console.WriteLine("N = "); N = Convert.ToInt32(Console.ReadLine());
+
+			S = 0;
+			k = i;
+			while (k <= N)
+			{
+				S += (1.0 / ((2 * k + 1) * (2 * k + 1)));
+				k++;
+			}
+			Console.WriteLine($"S = {Math.Round(S, 2)}");
+
+			S = 0;
+			k = i;
+			do
+			{
+				S += (1.0 / ((2 * k + 1) * (2 * k + 1)));
+				k++;
+			} while (k <= N);
+			Console.WriteLine($"S = {Math.Round(S, 2)}");
+
+			S = 0;
+			for (k = i; k <= N; k++)
+			{
+				S += (1.0 / ((2 * k + 1) * (2 * k + 1)));
+			}
+			Console.WriteLine($"S = {Math.Round(S, 2)}");
+
+			S = 0;
+			for (k = N; k >= i; k--)
+			{
+				S += (1.0 / ((2 * k + 1) * (2 * k + 1)));
+			}
+			Console.WriteLine($"S = {Math.Round(S, 2)}");
+
+		}
+	}
+}
